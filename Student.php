b2bbd47ac4fe71cded6bf129693aad2d84aad2fe
Wei Zhang <ink.wj.zhang@gmail.com>
2018-01-10 11:19:46 -0800

    Add comments to Student.php.
    Signed-off-by: Wei Zhang <ink.wj.zhang@gmail.com>

--- Student.php
+++ Student.php
@@ -6,9 +6,15 @@
  * Time: 11:04 AM
  */
 
+/**
+ * Class Student is a class which represents a bcit student.
+ */
 class Student
 {
 
+    /**
+     * Student constructor.
+     */
     function __construct() {
         $this->surname = '';
         $this->first_name = '';
@@ -16,14 +22,24 @@ class Student
         $this->grades = array();
     }
 
+    /**
+     * @param $which type of email
+     * @param $address address of email
+     */
     function add_email($which,$address) {
         $this->emails[$which] = $address;
     }
 
+    /**
+     * @param $grade grade score
+     */
     function add_grade($grade) {
         $this->grades[] = $grade;
     }
 
+    /**
+     * @return float|int average score
+     */
     function average() {
         $total = 0;
         foreach ($this->grades as $value)
@@ -31,6 +47,9 @@ class Student
         return $total / count($this->grades);
     }
 
+    /**
+     * @return string string to return
+     */
     function toString() {
         $result = $this->first_name . ' ' . $this->surname;
         $result .= ' ('.$this->average().")\n";
